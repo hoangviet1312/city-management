27e15069bfe12dd9ced23b968f3b5a57e6805780
hoangviet <hoangviet1312@gmail.com>
2018-08-28 13:04:57 +0700

    update customercontroller

--- src/main/java/com/codegym/cms/controller/CustomerController.java
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -5,14 +5,13 @@ import com.codegym.cms.model.Province;
 import com.codegym.cms.service.CustomerService;
 import com.codegym.cms.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.awt.print.Pageable;
+import java.util.Optional;
 
 @Controller
 public class CustomerController {
@@ -24,7 +23,7 @@ public class CustomerController {
     private ProvinceService provinceService;
 
     @ModelAttribute("provinces")
-    public Iterable<Province> provinces(){
+    public Iterable<Province> provinces() {
         return provinceService.findAll();
     }
 
@@ -45,29 +44,35 @@ public class CustomerController {
     }
 
     @GetMapping("/customers")
-    public ModelAndView listCustomers(org.springframework.data.domain.Pageable pageable){
-        Iterable<Customer> customers = customerService.findAll(pageable);
+    public ModelAndView listCustomers(@RequestParam("s") Optional<String> s,
+                                      org.springframework.data.domain.Pageable pageable) {
+        Page<Customer> customers;
+        if (s.isPresent()) {
+            customers = customerService.findAllByFirstNameContaining(s.get(), pageable);
+        } else {
+            customers = customerService.findAll(pageable);
+        }
         ModelAndView modelAndView = new ModelAndView("/list");
         modelAndView.addObject("customers", customers);
         return modelAndView;
     }
 
     @GetMapping("/edit-customer/{id}")
-    public ModelAndView showEditForm(@PathVariable Long id){
+    public ModelAndView showEditForm(@PathVariable Long id) {
         Customer customer = customerService.findById(id);
-        if(customer != null) {
+        if (customer != null) {
             ModelAndView modelAndView = new ModelAndView("/edit");
             modelAndView.addObject("customer", customer);
             return modelAndView;
 
-        }else {
+        } else {
             ModelAndView modelAndView = new ModelAndView("/error.404");
             return modelAndView;
         }
     }
 
     @PostMapping("/edit-customer")
-    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer){
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer) {
         customerService.save(customer);
         ModelAndView modelAndView = new ModelAndView("/edit");
         modelAndView.addObject("customer", customer);
@@ -76,21 +81,21 @@ public class CustomerController {
     }
 
     @GetMapping("/delete-customer/{id}")
-    public ModelAndView showDeleteForm(@PathVariable Long id){
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
         Customer customer = customerService.findById(id);
-        if(customer != null) {
+        if (customer != null) {
             ModelAndView modelAndView = new ModelAndView("/delete");
             modelAndView.addObject("customer", customer);
             return modelAndView;
 
-        }else {
+        } else {
             ModelAndView modelAndView = new ModelAndView("/error.404");
             return modelAndView;
         }
     }
 
     @PostMapping("/delete-customer")
-    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer) {
         customerService.remove(customer.getId());
         return "redirect:customers";
     }
