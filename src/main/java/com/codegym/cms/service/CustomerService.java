e75487138534bffb33486baa5cd8adc2a0f28369
hoangviet <hoangviet1312@gmail.com>
2018-08-28 12:40:19 +0700

    update CustomerService

--- src/main/java/com/codegym/cms/service/CustomerService.java
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -3,9 +3,10 @@ package com.codegym.cms.service;
 import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
-    Page<Customer> findAll();
+    Page<Customer> findAll(Pageable pageable);
 
     Customer findById(Long id);
 
