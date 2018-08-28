e0bc5ed524972b9a829d26cc88aaca574d0f27bf
hoangviet <hoangviet1312@gmail.com>
2018-08-28 12:42:46 +0700

    update appconfig

--- src/main/java/com/codegym/cms/AppConfig.java
+++ src/main/java/com/codegym/cms/AppConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -38,6 +39,7 @@ import java.util.Properties;
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
+@EnableSpringDataWebSupport
 @ComponentScan("com.codegym.cms")
 @EnableJpaRepositories("com.codegym.cms.repository")
 public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware  {
